994b04b2094792437874412ef78a8229e0340ca8
Zheka <korsakasPr@gmail.com>
2017-03-20 17:16:49 +0300

    The first commit

---
+++ InstinctoolsProj/ConsoleTestApp/Entity/Contexts/CarContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using ConsoleTestApp.Entity.Entities;
+namespace ConsoleTestApp.Entity.Contexts
+{
+   public class CarContext: DbContext
+    {
+        public CarContext(string connectionString = "name=Instinctools.WorkersDatabaseConnection") : base(connectionString) { }
+        public CarContext() { }
+
+        public virtual DbSet<Worker> Workers { set; get; }
+    }
+}
---
+++ InstinctoolsProj/ConsoleTestApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleTestApp.Entity.Contexts;
+using ConsoleTestApp.Entity.Entities;
+namespace ConsoleTestApp
+{
+   public class Program
+    {
+       public static void Main(string[] args)
+        {
+            using (CarContext context = new CarContext()) {
+                foreach (var worker in context.Workers) {
+                    Console.WriteLine(worker.ToString());
+                }
+            }
+
+
+                Console.ReadKey();
+        }
+    }
+}

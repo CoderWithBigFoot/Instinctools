12c04c37c82268f8624ba509e6e50e7f5950ca87
Zheka <korsakasPr@gmail.com>
2017-04-05 22:03:57 +0300

    Refactoring

--- BookStore/BookStore.WebUI/Models/AuthorViewModel.cs
+++ BookStore/BookStore.WebUI/Models/AuthorViewModel.cs
@@ -1,14 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace BookStore.WebUI.Models
 {
     public class AuthorViewModel
     {
+        [Required]
         public int Id { set; get; }
+
+        [Required]
+        [MinLength(2),MaxLength(100)]
         public string Name { set; get; }
+
+        [Required]
+        [MinLength(2),MaxLength(100)]
         public string Surname { set; get; }
     }
 }
--- BookStore/BookStore.WebUI/Models/BookViewModel.cs
+++ BookStore/BookStore.WebUI/Models/BookViewModel.cs
@@ -1,14 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace BookStore.WebUI.Models
 {
     public class BookViewModel
     {
+        [Required]
         public int Id { set; get; }
+        
+        [Required]
+        [MinLength(2),MaxLength(50)]
         public string Name { set; get; }
+
+        [Required]
+        [Range(1,int.MaxValue)]
         public int Pages { set; get; }
     }
 }

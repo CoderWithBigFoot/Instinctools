d51d1a9c49687de7d1dba5368e13f95ceefecfe0
Zheka <korsakasPr@gmail.com>
2017-04-07 11:06:59 +0300

    Refactoring

---
+++ BookStore/BookStore.Data.Database/BookStoreDbManagerContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookStore.Data.Contexts;
+
+namespace BookStore.Data.Database
+{
+    public class BookStoreDbManagerContext : BookStoreContext
+    {
+        public BookStoreDbManagerContext() : base("name = BookStoreDb") { }
+        public BookStoreDbManagerContext(string connectionString) : base(connectionString){ }
+    }
+}
